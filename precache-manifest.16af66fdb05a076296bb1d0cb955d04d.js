65821970b9c46f42f8f51c9a63b1cdc9843577b9
King <kingleer@38f9d372a2a7.ant.amazon.com>
2019-09-20 19:24:41 -0700

    Updates

--- precache-manifest.16af66fdb05a076296bb1d0cb955d04d.js
+++ precache-manifest.16af66fdb05a076296bb1d0cb955d04d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f7c96e9f639a07974b84c85def880aab",
+    "revision": "e70a9c4ca514dc1c76a621c6f160b67e",
     "url": "/ideagen/index.html"
   },
   {
-    "revision": "f1ba021de7b6cc7dedce",
+    "revision": "0be3ee74700e2a7e7f4c",
     "url": "/ideagen/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,15 +12,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ideagen/static/js/2.48dab418.chunk.js"
   },
   {
-    "revision": "f1ba021de7b6cc7dedce",
-    "url": "/ideagen/static/js/main.c02db2e7.chunk.js"
+    "revision": "0be3ee74700e2a7e7f4c",
+    "url": "/ideagen/static/js/main.4f6d46da.chunk.js"
   },
   {
     "revision": "a3bd8d162480e4e3a27c",
     "url": "/ideagen/static/js/runtime-main.98be4e59.js"
   },
   {
-    "revision": "25bf045ca257e971124f3997d89f321c",
-    "url": "/ideagen/static/media/logo.25bf045c.svg"
+    "revision": "8d898f3f1e7162bf5d4dbe3850857cb1",
+    "url": "/ideagen/static/media/lightbright.8d898f3f.png"
   }
 ]);
