e41342cbe0a867b660b4bf7a4a2ef971292b5b5f
King <kingleer@38f9d372a2a7.ant.amazon.com>
2019-09-20 19:05:49 -0700

    including build folder

---
+++ build/precache-manifest.7ba0efe92db5393840078f0a350f5330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165c313d5a24c143ee356060e1d8130f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e71a2e8f71fb99fdd0e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd6ba2f197907270b6b8",
+    "url": "/static/js/2.48dab418.chunk.js"
+  },
+  {
+    "revision": "0e71a2e8f71fb99fdd0e",
+    "url": "/static/js/main.401f3ab5.chunk.js"
+  },
+  {
+    "revision": "2af149371de81368d59b",
+    "url": "/static/js/runtime-main.6aaad5d6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
