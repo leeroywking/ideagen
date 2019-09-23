e627f65311d8e8b9982d1cb64dcd56b8942ea159
Lee-Roy King <leeroywking@gmail.com>
2019-09-22 18:14:20 -0700

    Implemented side modal for new input

---
+++ build/precache-manifest.7d73b13ac06076a5394c7b68cad18be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007f5b0d450cff97a9bd36a328c4557a",
+    "url": "/ideagen/index.html"
+  },
+  {
+    "revision": "3013dc3046ffc8e0d0b2",
+    "url": "/ideagen/static/css/main.70dc1ecf.chunk.css"
+  },
+  {
+    "revision": "0177cb0f913494183fa9",
+    "url": "/ideagen/static/js/2.89a1e74e.chunk.js"
+  },
+  {
+    "revision": "3013dc3046ffc8e0d0b2",
+    "url": "/ideagen/static/js/main.c3f88390.chunk.js"
+  },
+  {
+    "revision": "a3bd8d162480e4e3a27c",
+    "url": "/ideagen/static/js/runtime-main.98be4e59.js"
+  },
+  {
+    "revision": "6edb6d851950611fa4095e64ca8cee75",
+    "url": "/ideagen/static/media/lightbulbYellow.6edb6d85.png"
+  }
+]);
