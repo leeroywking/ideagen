117c596debdc28b814774adfb486ff699a108c11
Lee-Roy King <leeroywking@gmail.com>
2019-10-06 14:00:27 -0700

    looks like state is wired but its not doing anything

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './lightbulbYellow.png';
 import './App.css';
 import Pair from './resources/pair.js'
 import SlidingModal from './resources/slidingModal.js'
+import list from './hardList.js'
 
-function refreshPage(){
-  window.location.reload();
-}
+
+
 
 function App() {
+  const [globalState, setGlobalState]= useState({list})
+  function refreshPage(){
+    console.log(globalState.list)
+    globalState.list.shift()
+    console.log(globalState.list)
+    setGlobalState(globalState)
+    console.log('boop')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" onClick={()=> refreshPage()}/>
         <div className="titletext">Your suggestions for today are to think about this!!</div> 
         <p>
-        <Pair />
+        <Pair list={globalState.list} pair={[globalState.list[0],globalState.list[1]]}/>
         <SlidingModal />
+        {globalState.list}
         </p>
       </header>
     </div>
--- src/resources/pair.js
+++ src/resources/pair.js
@@ -1,44 +1,20 @@
 import React from 'react'
+import listContext from './context.js'
 
-let list = [
-"Web development",
-"Cyber Security",
-"Pashto",
-"AWS",
-"Cloud",
-"Bicycle riding",
-"Rally fighter",
-"Motocross",
-"Concrete",
-"Army",
-"Military",
-"Sigint",
-"Raspberry pi",
-"Accessibility",
-"Hiking",
-"Edge computing",
-"Comedy",
-"Tiny living",
-"Ultralight",
-"Web development",
-"Linux",
-"The thing to your left",
-"git"
-]
-
-function returnrandPair(){
-    let one = Math.floor(Math.random() * list.length);
-    let first = list.splice(one,1);
-    let two = Math.floor(Math.random() * list.length);
-    let second = list.splice(two, 1)
-    return(
+class ReturnRandPair extends React.Component {
+    constructor(props) {
+        super(props)
+    };
+render(){
+    return (
         <div>
-        <h1>{first}</h1>
-        <br/> and
-        <h1>{second} </h1>
+            <h1>{this.props.pair[0]}</h1>
+            <br /> and
+        <h1>{this.props.pair[1]} </h1>
         </div>
     )
 }
+}
 
 
-export default returnrandPair;
+export default ReturnRandPair;
