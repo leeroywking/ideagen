3b7115826a46927030d2d1a1daa3e007075247bc
Lee-Roy King <leeroywking@gmail.com>
2019-10-08 21:12:49 -0700

    Annotated app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Context from './resources/context.js'
 
 
 function App() {
+  // I don't think this will work this way I need to revisit this with context/redux I think
   const [globalState, setGlobalState]= useState({list})
   function shiftOff(){
     globalState.list.shift()
@@ -18,6 +19,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* best laid plans of mice and men :( */}
         <img src={logo} className="App-logo" alt="logo" onClick={()=> shiftOff()}/>
         <div className="titletext">Your suggestions for today are to think about this!!</div> 
         <p>
